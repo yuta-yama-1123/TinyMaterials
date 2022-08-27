7f9fbcbdae25a7b9247528d99eb951cbb78131d7
yuta-yama-1123 <yutaforios@gmail.com>
2022-08-27 23:51:30 +0900

    Initial Commit

---
+++ TinyMaterials/TinyMaterialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyMaterialsApp.swift
+//  TinyMaterials
+//
+//  Created by 山本祐太 on 2022/08/27.
+//
+
+import SwiftUI
+
+@main
+struct TinyMaterialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
